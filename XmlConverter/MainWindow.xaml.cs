0f3e069ee939ae66ddc81119433b6d4f2d188bd8
Rafaat Mir <rmir@pnimedia.com>
2018-12-15 16:19:05 -0800

    Convert XML to Word Document Initial Commit

--- XmlConverter/MainWindow.xaml.cs
+++ XmlConverter/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,10 +18,12 @@ using System.Windows.Shapes;
 using System.Xml;
 using System.Xml.XPath;
 using System.Xml.Xsl;
+using Word = Microsoft.Office.Interop.Word;
 using Spire.Doc;
 
 namespace XmlConverter
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -38,9 +41,49 @@ namespace XmlConverter
 
         private void ConvertXml(object sender, RoutedEventArgs e)
         {
-            Document doc = new Document();
-            doc.LoadFromFile("this.xml", FileFormat.Xml);
-            doc.SaveToFile("test.doc", FileFormat.Doc);
+            var xmlString = System.IO.File.ReadAllText("this.xml");
+
+            if (Word.IsChecked == true)
+            {
+                //var xmlString = System.IO.File.ReadAllText("this.xml");
+                //doc.LoadFromFile("this.xml", Spire.Doc.FileFormat.Txt);
+                //doc.LoadText("this.xml", Encoding.Default);
+                //Word.Application app = new Word.Application();
+                //Word.Document wDoc = app.Documents.Add();
+
+
+                //Word.Range rng = app.ActiveDocument.Range(start, end);
+                //rng.Text = xmlString;
+
+                //wDoc.SaveAs("wOutput.doc");
+                Word.Application app = new Word.Application();
+                Word.Document doc = app.Documents.Add();
+                try
+                {
+                    doc.Content.Font.Size = 12;
+                    doc.Content.Text = xmlString;
+
+                    doc.Save();
+                    doc.Close();
+
+                    //app.Visible = true;    //Optional
+                    // MessageBox.Show("File Saved successfully");
+                    this.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    Marshal.ReleaseComObject(app);
+                }
+            }
+            else if (PDF.IsChecked == true)
+            {
+
+            }
         }
     }
 }
